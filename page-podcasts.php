b3691265acb63f6e09eefb3750371449eecf36c3
rabb0018 <rabb0018@stud.kea.dk>
2021-04-22 16:33:43 +0200

    btihcc

---
+++ page-podcasts.php
@@ -0,0 +1,33 @@
+<?php
+/**
+Template for displaying front page
+ */
+
+get_header();
+?>
+
+	<div id="primary" class="content-area">
+		<main id="main" class="site-main">
+
+			<?php
+
+			// Start the Loop.
+			while ( have_posts() ) :
+				the_post();
+
+				get_template_part( 'template-parts/content/content', 'page' );
+
+				// If comments are open or we have at least one comment, load up the comment template.
+				if ( comments_open() || get_comments_number() ) {
+					comments_template();
+				}
+
+			endwhile; // End the loop.
+			?>
+
+		</main><!-- #main -->
+	</div><!-- #primary -->
+
+<?php
+get_footer();
+
